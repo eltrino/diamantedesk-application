489486d7fd836572a46a9b77f5b4175bad62f743
Ruslan Voitenko <psw1986@gmail.com>
2014-10-22 20:14:47 +0300

    #DIAM-288 Rename configuration nodes in bundles due to new names

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -31,7 +31,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('eltrino_email_processing');
+        $rootNode = $treeBuilder->root('diamante_email_processing');
 
         SettingsBuilder::append(
             $rootNode,
