7aa910e201b34eb7a1dea1bf7b1d55f26891471d
Alexandr Kolomiec <alexandr.kolomiec@eltrino.com>
2015-02-12 19:40:36 +0200

    DIAM-529 call reset service in actions

--- Controller/ResetPasswordController.php
+++ Controller/ResetPasswordController.php
@@ -14,25 +14,35 @@
  */
 namespace Diamante\FrontBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations\Post;
+use Symfony\Component\HttpFoundation\Request;
 
-class ResetPasswordController
+class ResetPasswordController extends Controller
 {
     /**
      * Reset password action
      * @Post("/password/reset", name="diamante_front_reset_password")
      */
-    public function resetAction(){
+    public function resetAction(Request $request)
+    {
+        $email = $request->get('email');
+        $resetService = $this->container->get('diamante.front.reset_password');
+        $resetService->reset($email);
+        return new Response();
     }
 
     /**
      * Update password action
      * @Post("password/update", name="diamante_front_update_password")
      */
-    public function updateAction()
+    public function updateAction(Request $request)
     {
-
-        $service = $this->container->get('diamante.front.reset_password');
-        $service->reset('max@gmail.com');
+        $hash = $request->get('hash');
+        $password = $request->get('password');
+        $resetService = $this->container->get('diamante.front.reset_password');
+        $resetService->changePassword($hash, $password);
+        return new Response();
     }
 }
