ddef0153a0debd777511f65bbe9e0e37ff2ec3ed
Konstantin Stupak <kosmass.zt@gmail.com>
2015-02-24 18:48:09 +0200

    DIAM-565: Attachment service

---
+++ Api/Internal/AttachmentServiceImpl.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Copyright (c) 2014 Eltrino LLC (http://eltrino.com)
+ *
+ * Licensed under the Open Software License (OSL 3.0).
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://opensource.org/licenses/osl-3.0.php
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@eltrino.com so we can send you a copy immediately.
+ */
+
+namespace Diamante\DeskBundle\Api\Internal;
+
+use Diamante\DeskBundle\Api\AttachmentService;
+use Diamante\DeskBundle\Model\Attachment\AttachmentRepository;
+use Diamante\DeskBundle\Model\Attachment\Manager;
+
+class AttachmentServiceImpl implements AttachmentService
+{
+    /**
+     * @var \Diamante\DeskBundle\Model\Attachment\AttachmentRepository
+     */
+    private $repository;
+    /**
+     * @var \Diamante\DeskBundle\Model\Attachment\Manager
+     */
+    private $manager;
+
+    public function __construct(Manager $manager, AttachmentRepository $repository)
+    {
+        $this->repository = $repository;
+        $this->manager = $manager;
+    }
+
+    /**
+     * @param $hash
+     * @return \Diamante\DeskBundle\Entity\Attachment|null
+     */
+    public function getByHash($hash)
+    {
+        return $this->repository->getByHash($hash);
+    }
+}
