568152f68b268dab77549b8ed305d787136d0acf
Alexandr Kolomiec <alexandr.kolomiec@eltrino.com>
2015-03-18 18:48:40 +0200

    DIAM-530 add ticket attachment tests

--- Tests/Functional/TicketApiTest.php
+++ Tests/Functional/TicketApiTest.php
@@ -34,7 +34,7 @@ class TicketApiTest extends ApiTestCase
         $this->command = new ApiCommand();
     }
 
-    public function testList()
+    public function testListTickets()
     {
         $this->getAll('diamante_ticket_api_service_oro_list_all_tickets');
     }
@@ -60,6 +60,12 @@ class TicketApiTest extends ApiTestCase
         $this->get('diamante_ticket_api_service_oro_load_ticket', $this->command);
     }
 
+    public function testGetTicketByKey()
+    {
+        $this->command->urlParameters = array('key' => 'BRANCHE-1');
+        $this->get('diamante_ticket_api_service_oro_load_ticket_by_key', $this->command);
+    }
+
     public function testUpdateTicket()
     {
         $this->command->urlParameters = array('id' => 2);
@@ -78,5 +84,70 @@ class TicketApiTest extends ApiTestCase
         $this->command->urlParameters = array('id' => 1);
         $this->delete('diamante_ticket_api_service_oro_delete_ticket', $this->command);
         $this->get('diamante_ticket_api_service_oro_load_ticket', $this->command, Codes::HTTP_NOT_FOUND);
+    }
+
+    public function testDeleteTicketByKey()
+    {
+        $this->command->urlParameters = array('key' => 'BRANCHE-1');
+        $this->delete('diamante_ticket_api_service_oro_delete_ticket_by_key', $this->command);
+        $this->get('diamante_ticket_api_service_oro_load_ticket_by_key', $this->command, Codes::HTTP_NOT_FOUND);
+    }
+
+    public function testListTicketAttachments()
+    {
+        $this->command->urlParameters = array('id' => 2);
+        $this->get('diamante_ticket_api_service_oro_list_ticket_attachments', $this->command);
+    }
+
+    public function testAddAttachmentToTicket()
+    {
+        $attachment = file_get_contents(
+            dirname(__FILE__) . DIRECTORY_SEPARATOR . 'fixture' . DIRECTORY_SEPARATOR . 'test.jpg'
+        );
+        $this->command->urlParameters = array('ticketId' => 2);
+        $this->command->requestParameters = array(
+            'attachmentsInput' => array(
+                array(
+                    'filename' => 'test.jpg',
+                    'content'  => base64_encode($attachment)
+                )
+            )
+        );
+
+        return $this->post('diamante_ticket_api_service_oro_add_attachments_for_ticket', $this->command);
+    }
+
+    /**
+     * @depends testAddAttachmentToTicket
+     *
+     * @param \Symfony\Component\HttpFoundation\Response $response
+     */
+    public function testGetTicketAttachment($response)
+    {
+        $attachmentId = $this->getByKey($response, 'id');
+        $this->command->urlParameters = array('ticketId' => 2, 'attachmentId' => $attachmentId);
+        $this->get('diamante_ticket_api_service_oro_get_ticket_attachment', $this->command);
+    }
+
+    /**
+     * @depends testAddAttachmentToTicket
+     *
+     * @param \Symfony\Component\HttpFoundation\Response $response
+     */
+    public function testDeleteTicketAttachment($response)
+    {
+        $attachmentId = $this->getByKey($response, 'id');
+        $this->command->urlParameters = array('ticketId' => 2, 'attachmentId' => $attachmentId);
+        $this->delete('diamante_ticket_api_service_oro_remove_attachment_from_ticket', $this->command);
+        $this->get('diamante_ticket_api_service_oro_get_ticket_attachment', $this->command, Codes::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Response $response
+     * @param string                                     $key
+     */
+    protected function getByKey($response, $key)
+    {
+        return self::jsonToArray($response->getContent())[0][$key];
     }
 }
