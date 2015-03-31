3b9c1afc3829df0af1ea9114b52db5e2d1d6f81d
Konstantin Stupak <kosmass.zt@gmail.com>
2015-03-31 14:21:29 +0300

    DIAM-660: Moved Doctrine DBAL type to User Bundle

--- DiamanteUserBundle.php
+++ DiamanteUserBundle.php
@@ -2,8 +2,18 @@
 
 namespace Diamante\UserBundle;
 
+use Doctrine\DBAL\Types\Type;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DiamanteUserBundle extends Bundle
 {
+    public function boot()
+    {
+        if (!Type::hasType('user_type')) {
+            Type::addType(
+                'user_type',
+                'Diamante\UserBundle\Infrastructure\Persistence\Doctrine\DBAL\Types\TicketUserType'
+            );
+        }
+    }
 }
