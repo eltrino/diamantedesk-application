b982f6db51e6c0fe39c5b164233f7216787d6abc
Alexandr Kolomiec <alexandr.kolomiec@eltrino.com>
2015-04-13 22:11:23 +0300

    DIAM-681 add test for email notifier

--- Tests/Infrastructure/Ticket/Notifications/EmailNotifierTest.php
+++ Tests/Infrastructure/Ticket/Notifications/EmailNotifierTest.php
@@ -115,12 +115,19 @@ class EmailNotifierTest extends \PHPUnit_Framework_TestCase
             new Source(Source::WEB), new Priority(Priority::PRIORITY_MEDIUM), new Status(Status::NEW_ONE)
         );
         $notification = new Notification(
-            (string) $ticketUniqueId, $author, 'Header', 'Subject', new \ArrayIterator(array('key' => 'value')), array('file.ext')
+            (string)$ticketUniqueId,
+            $author,
+            'Header',
+            'Subject',
+            new \ArrayIterator(array('key' => 'value')),
+            array('file.ext')
         );
 
         $message = new \Swift_Message();
 
-        $this->nameFormatter->expects($this->once())->method('format')->with($this->diamanteUser)->will($this->returnValue('First Last'));
+        $this->nameFormatter->expects($this->once())->method('format')->with($this->diamanteUser)->will(
+            $this->returnValue('First Last')
+        );
         $this->mailer->expects($this->once())->method('createMessage')->will($this->returnValue($message));
         $this->ticketRepository->expects($this->once())->method('getByUniqueId')->with($ticketUniqueId)
             ->will($this->returnValue($ticket));
@@ -132,16 +139,24 @@ class EmailNotifierTest extends \PHPUnit_Framework_TestCase
             ->will($this->returnValue($this->diamanteUser));
 
         $this->templateResolver->expects($this->any())->method('resolve')->will(
-            $this->returnValueMap(array(
-                array($notification, TemplateResolver::TYPE_TXT, 'txt.template.html'),
-                array($notification, TemplateResolver::TYPE_HTML, 'html.template.html')
-            ))
+            $this->returnValueMap(
+                array(
+                    array($notification, TemplateResolver::TYPE_TXT, 'txt.template.html'),
+                    array($notification, TemplateResolver::TYPE_HTML, 'html.template.html')
+                )
+            )
         );
 
         $optionsConstraint = $this->logicalAnd(
-            $this->arrayHasKey('changes'), $this->arrayHasKey('attachments'), $this->arrayHasKey('user'), $this->arrayHasKey('header'),
-            $this->contains($notification->getChangeList()), $this->contains($notification->getAttachments()),
-            $this->contains('First Last'), $this->contains($notification->getHeaderText()));
+            $this->arrayHasKey('changes'),
+            $this->arrayHasKey('attachments'),
+            $this->arrayHasKey('user'),
+            $this->arrayHasKey('header'),
+            $this->contains($notification->getChangeList()),
+            $this->contains($notification->getAttachments()),
+            $this->contains('First Last'),
+            $this->contains($notification->getHeaderText())
+        );
 
         $this->twig->expects($this->at(0))->method('render')->with('txt.template.html', $optionsConstraint)
             ->will($this->returnValue('Rendered TXT template'));
@@ -151,25 +166,142 @@ class EmailNotifierTest extends \PHPUnit_Framework_TestCase
         $this->mailer->expects($this->once())->method('send')->with(
             $this->logicalAnd(
                 $this->isInstanceOf('\Swift_Message'),
-                $this->callback(function(\Swift_Message $other) use($notification) {
-                    $to = $other->getTo();
-                    return false !== strpos($other->getSubject(), $notification->getSubject())
+                $this->callback(
+                    function (\Swift_Message $other) use ($notification) {
+                        $to = $other->getTo();
+
+                        return false !== strpos($other->getSubject(), $notification->getSubject())
                         && false !== strpos($other->getSubject(), 'KEY-1')
                         && false !== strpos($other->getBody(), 'Rendered TXT template')
-                        && array_key_exists('reporter@host.com', $to) && array_key_exists('assignee@host.com', $to)
+                        && array_key_exists('reporter@host.com', $to)
+                        && array_key_exists('assignee@host.com', $to)
                         && $other->getHeaders()->has('References')
                         && false !== strpos($other->getHeaders()->get('References'), 'id_1@host.com')
                         && false !== strpos($other->getHeaders()->get('References'), 'id_2@host.com');
-                })
+                    }
+                )
             )
         );
 
         $this->messageReferenceRepository->expects($this->once())->method('findAllByTicket')->with($ticket)
             ->will(
-                $this->returnValue(array(
-                        new MessageReference('id_1@host.com', $ticket), new MessageReference('id_2@host.com', $ticket)
+                $this->returnValue(
+                    array(
+                        new MessageReference('id_1@host.com', $ticket),
+                        new MessageReference('id_2@host.com', $ticket)
                     )
-                ));
+                )
+            );
+
+        $this->messageReferenceRepository->expects($this->once())->method('store')->with(
+            $this->logicalAnd(
+                $this->isInstanceOf('\Diamante\DeskBundle\Model\Ticket\EmailProcessing\MessageReference')
+            )
+        );
+
+        $notifier = new EmailNotifier(
+            $this->twig, $this->mailer, $this->templateResolver, $this->ticketRepository,
+            $this->messageReferenceRepository, $this->userService, $this->nameFormatter,
+            $this->diamanteUserRepository, $this->senderEmail, $this->senderHost
+        );
+
+        $notifier->notify($notification);
+    }
+
+    public function testNotifyWithEmptyAuthor()
+    {
+        $ticketUniqueId = UniqueId::generate();
+        $reporter = new UserAdapter(1, UserAdapter::TYPE_DIAMANTE);
+        $assignee = new User();
+        $assignee->setId(2);
+        $assignee->setEmail('assignee@host.com');
+        $author = null;
+        $branch = new Branch('KEY', 'Name', 'Description');
+        $ticket = new Ticket(
+            $ticketUniqueId, new TicketSequenceNumber(1), 'Subject', 'Description', $branch, $reporter, $assignee,
+            new Source(Source::WEB), new Priority(Priority::PRIORITY_MEDIUM), new Status(Status::NEW_ONE)
+        );
+        $notification = new Notification(
+            (string)$ticketUniqueId,
+            $author,
+            'Header',
+            'Subject',
+            new \ArrayIterator(array('key' => 'value')),
+            array('file.ext')
+        );
+
+        $message = new \Swift_Message();
+
+        $this->diamanteUserRepository->expects($this->once())->method('get')->will(
+            $this->returnValue($this->diamanteUser)
+        );
+        $this->nameFormatter->expects($this->once())->method('format')->with($this->diamanteUser)->will(
+            $this->returnValue('First Last')
+        );
+        $this->mailer->expects($this->once())->method('createMessage')->will($this->returnValue($message));
+        $this->ticketRepository->expects($this->once())->method('getByUniqueId')->with($ticketUniqueId)
+            ->will($this->returnValue($ticket));
+
+        $this->userService
+            ->expects($this->once())
+            ->method('getByUser')
+            ->with($this->equalTo(new UserAdapter(1, UserAdapter::TYPE_DIAMANTE)))
+            ->will($this->returnValue($this->diamanteUser));
+
+        $this->templateResolver->expects($this->any())->method('resolve')->will(
+            $this->returnValueMap(
+                array(
+                    array($notification, TemplateResolver::TYPE_TXT, 'txt.template.html'),
+                    array($notification, TemplateResolver::TYPE_HTML, 'html.template.html')
+                )
+            )
+        );
+
+        $optionsConstraint = $this->logicalAnd(
+            $this->arrayHasKey('changes'),
+            $this->arrayHasKey('attachments'),
+            $this->arrayHasKey('user'),
+            $this->arrayHasKey('header'),
+            $this->contains($notification->getChangeList()),
+            $this->contains($notification->getAttachments()),
+            $this->contains('First Last'),
+            $this->contains($notification->getHeaderText())
+        );
+
+        $this->twig->expects($this->at(0))->method('render')->with('txt.template.html', $optionsConstraint)
+            ->will($this->returnValue('Rendered TXT template'));
+        $this->twig->expects($this->at(1))->method('render')->with('html.template.html', $optionsConstraint)
+            ->will($this->returnValue('Rendered HTML template'));
+
+        $this->mailer->expects($this->once())->method('send')->with(
+            $this->logicalAnd(
+                $this->isInstanceOf('\Swift_Message'),
+                $this->callback(
+                    function (\Swift_Message $other) use ($notification) {
+                        $to = $other->getTo();
+
+                        return false !== strpos($other->getSubject(), $notification->getSubject())
+                        && false !== strpos($other->getSubject(), 'KEY-1')
+                        && false !== strpos($other->getBody(), 'Rendered TXT template')
+                        && array_key_exists('reporter@host.com', $to)
+                        && array_key_exists('assignee@host.com', $to)
+                        && $other->getHeaders()->has('References')
+                        && false !== strpos($other->getHeaders()->get('References'), 'id_1@host.com')
+                        && false !== strpos($other->getHeaders()->get('References'), 'id_2@host.com');
+                    }
+                )
+            )
+        );
+
+        $this->messageReferenceRepository->expects($this->once())->method('findAllByTicket')->with($ticket)
+            ->will(
+                $this->returnValue(
+                    array(
+                        new MessageReference('id_1@host.com', $ticket),
+                        new MessageReference('id_2@host.com', $ticket)
+                    )
+                )
+            );
 
         $this->messageReferenceRepository->expects($this->once())->method('store')->with(
             $this->logicalAnd(
