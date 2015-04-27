a414e5d5deb9d77ce7c421d65395cfeeb05a70c6
Vladimir Polischuk <vladimir.polischuk@eltrino.com>
2015-04-27 17:30:00 +0300

    DIAM-707 Entity Branch and Ticket should be added to Oro search

---
+++ EventListener/Search/PrepareResultItemListener.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Diamante\DeskBundle\EventListener\Search;
+
+use Diamante\DeskBundle\Entity\Ticket;
+use Doctrine\ORM\EntityManager;
+use Oro\Bundle\SearchBundle\Engine\ObjectMapper;
+use Oro\Bundle\SearchBundle\Event\PrepareResultItemEvent;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class PrepareResultItemListener
+{
+    const TITLE_PART_SEPARATOR = ' - ';
+
+    /**
+     * @var ContainerInterface $container
+     */
+    private $container;
+
+    /**
+     * @var ObjectMapper
+     */
+    private $mapper;
+
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * @param ContainerInterface $container
+     * @param ObjectMapper $mapper
+     * @param EntityManager $em
+     */
+    public function __construct(
+        ContainerInterface $container,
+        ObjectMapper $mapper,
+        EntityManager $em
+    ) {
+        $this->container = $container;
+        $this->mapper = $mapper;
+        $this->em = $em;
+    }
+
+    public function process(PrepareResultItemEvent $event)
+    {
+
+        $entity = $event->getEntity();
+
+        if (!$entity instanceof Ticket) {
+            return;
+        }
+
+        $item = $event->getResultItem();
+        $name = $item->getEntityName();
+
+        $routeParameters = $this->mapper->getEntityMapParameter($name, 'route');
+        $routeData = array();
+        foreach ($routeParameters['parameters'] as $parameter => $field) {
+            $routeData[$parameter] = $entity->getKey();
+            break;
+        }
+
+        $router = $this->container->get('router');
+
+        $url = $router->generate(
+            $routeParameters['name'],
+            $routeData,
+            true
+        );
+
+        $item->setRecordUrl($url);
+
+        $title = $entity->getKey() . static::TITLE_PART_SEPARATOR . $item->getRecordTitle();
+        $item->setRecordTitle($title);
+    }
+}
