153dd1e3fb421963fc332b88a02375f73d30c3c4
Kostyantyn Stupak <kosmass.zt@gmail.com>
2015-07-01 19:14:16 +0300

    DIAM-873: Reorganize installer steps - override of Oro setup step to disable sample data loading.

---
+++ src/Diamante/DistributionBundle/Form/SetupType.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Diamante\DistributionBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class SetupType extends AbstractType
+{
+    protected $dataClass;
+
+    public function __construct($dataClass)
+    {
+        $this->dataClass = $dataClass;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'application_url',
+                'text',
+                array(
+                    'label'    => 'form.setup.application_url',
+                    'mapped'   => false,
+                    'required' => false,
+                )
+            )
+            ->add(
+                'organization_name',
+                'text',
+                array(
+                    'label'    => 'form.setup.organization_name',
+                    'mapped'   => false,
+                    'required' => false,
+                )
+            )
+            ->add(
+                'username',
+                'text',
+                array(
+                    'label' => 'form.setup.username',
+                )
+            )
+            ->add(
+                'plainPassword',
+                'repeated',
+                array(
+                    'type'           => 'password',
+                    'invalid_message' => 'The password fields must match.',
+                    'first_options'  => array('label' => 'form.setup.password'),
+                    'second_options' => array('label' => 'form.setup.password_re'),
+                )
+            )
+            ->add(
+                'email',
+                'email',
+                array(
+                    'label' => 'form.setup.email',
+                )
+            )
+            ->add(
+                'firstName',
+                'text',
+                array(
+                    'label' => 'form.setup.firstname',
+                )
+            )
+            ->add(
+                'lastName',
+                'text',
+                array(
+                    'label' => 'form.setup.lastname',
+                )
+            );
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class'              => $this->dataClass,
+                'validation_groups'       => array('Registration', 'Default'),
+                'dynamic_fields_disabled' => true
+            )
+        );
+    }
+
+    public function getName()
+    {
+        return 'oro_installer_setup';
+    }
+}
---
+++ src/Diamante/DistributionBundle/Installer/Process/Step/AdministrationSetupStep.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: s3nt1nel
+ * Date: 7/1/15
+ * Time: 4:44 PM
+ */
+
+namespace Diamante\DistributionBundle\Installer\Process\Step;
+
+
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+use Oro\Bundle\InstallerBundle\Process\Step\AbstractStep;
+use Oro\Bundle\UserBundle\Migrations\Data\ORM\LoadAdminUserData;
+use Sylius\Bundle\FlowBundle\Process\Context\ProcessContextInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdministrationSetupStep extends AbstractStep
+{
+    public function displayAction(ProcessContextInterface $context)
+    {
+        $form = $this->createForm('diamante_installer_setup');
+
+        /** @var ConfigManager $configManager */
+        $configManager = $this->get('oro_config.global');
+
+        $form->get('organization_name')->setData($configManager->get('oro_ui.organization_name'));
+        $form->get('application_url')->setData($configManager->get('oro_ui.application_url'));
+
+        return $this->render(
+            'DiamanteDistributionBundle:Process/Step:setup.html.twig',
+            array(
+                'form' => $form->createView()
+            )
+        );
+    }
+
+    public function forwardAction(ProcessContextInterface $context)
+    {
+        $adminUser = $this
+            ->getDoctrine()
+            ->getRepository('OroUserBundle:User')
+            ->findOneBy(array('username' => LoadAdminUserData::DEFAULT_ADMIN_USERNAME));
+
+        if (!$adminUser) {
+            throw new \RuntimeException("Admin user wasn't loaded in fixtures.");
+        }
+
+        $form = $this->createForm('diamante_installer_setup');
+        $form->setData($adminUser);
+
+        $form->handleRequest($this->getRequest());
+
+        if ($form->isValid()) {
+            $context->getStorage()->set(
+                'loadFixtures',
+                false
+            );
+
+            $this->get('oro_user.manager')->updateUser($adminUser);
+
+            /** @var ConfigManager $configManager */
+            $configManager           = $this->get('oro_config.global');
+            $defaultOrganizationName = $configManager->get('oro_ui.organization_name');
+            $organizationName        = $form->get('organization_name')->getData();
+            if (!empty($organizationName) && $organizationName !== $defaultOrganizationName) {
+                $organizationManager = $this->get('oro_organization.organization_manager');
+                $organization        = $organizationManager->getOrganizationByName('default');
+                $organization->setName($organizationName);
+
+                $organizationManager->updateOrganization($organization);
+            }
+
+            $defaultAppURL       = $configManager->get('oro_ui.application_url');
+            $applicationURL      = $form->get('application_url')->getData();
+            if (!empty($applicationURL) && $applicationURL !== $defaultAppURL) {
+                $configManager->set('oro_ui.application_url', $applicationURL);
+            }
+            $configManager->flush();
+
+            return $this->complete();
+        }
+
+        return $this->render(
+            'OroInstallerBundle:Process/Step:setup.html.twig',
+            array(
+                'form' => $form->createView()
+            )
+        );
+    }
+}
