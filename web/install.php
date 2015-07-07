2896b0ce2b563b567b271fe0a0920cb5966d322c
Eugene Tereschenko <i@jman.info>
2015-07-07 15:17:39 +0300

    DIAM-180 favicons

--- web/install.php
+++ web/install.php
@@ -83,8 +83,32 @@ function iterateRequirements(array $collection)
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title><?php echo $translator->trans('title'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="manifest" href="bundles/diamantedistribution/img/ico/manifest.json">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="bundles/diamantedistribution/img/ico/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="bundles/diamantedistribution/img/ico/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="bundles/diamantedistribution/img/ico/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="bundles/diamantedistribution/img/ico/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="bundles/diamantedistribution/img/ico/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="bundles/diamantedistribution/img/ico/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="bundles/diamantedistribution/img/ico/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="bundles/diamantedistribution/img/ico/apple-touch-icon-152x152.png" />
+
+    <link rel="icon" type="image/png" href="bundles/diamantedistribution/img/ico/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="bundles/diamantedistribution/img/ico/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="bundles/diamantedistribution/img/ico/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="bundles/diamantedistribution/img/ico/favicon-194x194.png" sizes="194x194" />
+    <link rel="icon" type="image/png" href="bundles/diamantedistribution/img/ico/favicon-194x194.png" sizes="194x194" />
+    <link rel="icon" type="image/png" href="bundles/diamantedistribution/img/ico/favicon-194x194.png" sizes="194x194" />
+
+    <meta name="application-name" content="<?php echo $translator->trans('title'); ?>"/>
+    <meta name="msapplication-TileColor" content="#f6fbff" />
+    <meta name="msapplication-TileImage" content="bundles/diamantedistribution/img/ico/mstile-144x144.png" />
+    <meta name="theme-color" content="#ffffff">
+
     <link rel="stylesheet" type="text/css" href="bundles/oroinstaller/css/style.css" />
     <link rel="stylesheet" type="text/css" href="bundles/diamantedistribution/css/style.css" />
+
     <script type="text/javascript" src="bundles/oroinstaller/lib/jquery-2.0.3.min.js"></script>
     <script type="text/javascript">
         $(function() {
