d1a0b81d0639f787dff64858c8e86e377ca9df33
Vladimir Polischuk <vladimir.polischuk@eltrino.com>
2015-07-09 17:47:06 +0300

    Revert "DIAM-593 Remove EmbeddedForm migrations"
    This reverts commit 562b4620f66b020ec650c257895b2b6cf3d6b552.

---
+++ Migrations/Schema/v1_0/DiamanteEmbeddedFormBundle.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Copyright (c) 2014 Eltrino LLC (http://eltrino.com)
+ *
+ * Licensed under the Open Software License (OSL 3.0).
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://opensource.org/licenses/osl-3.0.php
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@eltrino.com so we can send you a copy immediately.
+ */
+namespace Diamante\EmbeddedFormBundle\Migrations\Schema\v1_0;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
+
+class DiamanteEmbeddedFormBundle implements Migration, ExtendExtensionAwareInterface
+{
+    /** @var ExtendExtension */
+    protected $extendExtension;
+
+    /**
+     * @inheritdoc
+     */
+    public function setExtendExtension(ExtendExtension $extendExtension)
+    {
+        $this->extendExtension = $extendExtension;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $this->extendExtension->addManyToOneRelation(
+            $schema,
+            'oro_embedded_form',
+            'branch',
+            'diamante_branch',
+            'name',
+            ['extend' => ['owner' => ExtendScope::OWNER_CUSTOM, 'is_extend' => true]]
+        );
+    }
+}
