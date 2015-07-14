c42a0188b91546e5f92dda6ac8858f16f0749e46
Vladimir Polischuk <vladimir.polischuk@eltrino.com>
2015-07-14 16:12:13 +0300

    Fix attachment input style

--- Form/Type/DiamanteEmbeddedFormType.php
+++ Form/Type/DiamanteEmbeddedFormType.php
@@ -203,6 +203,94 @@ span.validation-failed {
     line-height: 1.1em;
     margin: 3px 0 6px 0;
 }
+#file-box {
+    margin-top: 15px;
+    box-sizing: border-box;
+    color: rgb(68, 68, 68);
+    cursor: default;
+    float: left;
+    height: 28px;
+    position: relative;
+    vertical-align: middle;
+    width: 294.375px;
+    perspective-origin: 147.1875px 14px;
+    transform-origin: 147.1875px 14px;
+    background: rgb(255, 255, 255) none repeat scroll 0% 0% / auto padding-box border-box;
+    border: 1px solid rgb(204, 204, 204);
+    border-radius: 3px 3px 3px 3px;
+    font: normal normal normal normal 13px/28px Helvetica, Arial, sans-serif;
+    outline: rgb(68, 68, 68) none 0px;
+    overflow: hidden;
+}/*#file-box*/
+
+#file-box label {
+    display: none;
+}
+
+#diamante_embedded_form_attachmentsInput {
+    bottom: 0px;
+    box-sizing: content-box;
+    color: rgb(51, 51, 51);
+    cursor: default;
+    display: block;
+    height: 25px;
+    opacity: 0;
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    vertical-align: middle;
+    width: 246px;
+    z-index: 0;
+    align-items: baseline;
+    align-self: stretch;
+    perspective-origin: 123px 12.5px;
+    transform-origin: 123px 12.5px;
+    background: rgba(0, 0, 0, 0) none repeat scroll 0% 0% / auto padding-box border-box;
+    border: 0px none rgb(51, 51, 51);
+    font: normal normal normal normal 13px/30px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    outline: rgb(51, 51, 51) none 0px;
+    padding: 0px;
+}/*#diamante_embedded_form_attachmentsInput*/
+
+#file-title-first {
+    box-sizing: border-box;
+    color: rgb(68, 68, 68);
+    cursor: default;
+    display: block;
+    float: left;
+    height: 28px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    width: 202px;
+    perspective-origin: 101px 14px;
+    transform-origin: 101px 14px;
+    border: 0px none rgb(68, 68, 68);
+    font: normal normal normal normal 13px/28px Helvetica, Arial, sans-serif;
+    outline: rgb(68, 68, 68) none 0px;
+    overflow: hidden;
+    padding: 0px 10px;
+}/*#file-title-first*/
+
+#file-title-second {
+    box-sizing: border-box;
+    color: rgb(68, 68, 68);
+    cursor: pointer;
+    display: block;
+    float: left;
+    height: 28px;
+    width: 90.375px;
+    perspective-origin: 45.1875px 14px;
+    transform-origin: 45.1875px 14px;
+    background: rgb(245, 245, 245) linear-gradient(rgb(255, 255, 255), rgb(230, 230, 230)) repeat-x scroll 0% 0% / auto padding-box border-box;
+    border-top: 0px none rgb(68, 68, 68);
+    border-right: 0px none rgb(68, 68, 68);
+    border-bottom: 0px none rgb(68, 68, 68);
+    border-left: 1px solid rgb(204, 204, 204);
+    font: normal normal normal normal 13px/28px Helvetica, Arial, sans-serif;
+    outline: rgb(68, 68, 68) none 0px;
+    overflow: hidden;
+    padding: 0px 10px;
+}/*#file-title-second*/
 CSS;
     }
 
