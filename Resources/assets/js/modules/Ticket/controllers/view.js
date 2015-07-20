2dd0e3f587512c134eed2db8946ec8590ace19d5
Eugene Tereschenko <i@jman.info>
2015-07-20 17:36:36 +0300

    DIAM-816

--- Resources/assets/js/modules/Ticket/controllers/view.js
+++ Resources/assets/js/modules/Ticket/controllers/view.js
@@ -97,7 +97,7 @@ define(['app'], function(App){
           } else if (xhr.status === 301) {
             link = document.createElement('a');
             link.href = xhr.getResponseHeader('X-Location');
-            key = link.href.replace(model.urlRoot,'').replace('/','');
+            key = decodeURIComponent(link.href.replace(model.urlRoot,'').replace('/',''));
             App.trigger('message:show', {
               status:'info',
               text: 'This ticket was moved, so we automatically redirected you there'
