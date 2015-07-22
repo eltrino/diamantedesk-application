1b8b5f441d83cd68dfcffc391dbe79bea17099b0
Vladimir Polischuk <vladimir.polischuk@eltrino.com>
2015-07-22 18:34:08 +0300

    DIAM-812 Report configuration

--- Report/ChartType/GroupedLineChart.php
+++ Report/ChartType/GroupedLineChart.php
@@ -7,6 +7,7 @@ class GroupedLineChart extends AbstractChart
     const X_AXIS_ALIAS = 'x-axis';
     const Y_AXIS_ALIAS = 'y-axis';
     const Y_ITEMS_GROUP_NAME = 'group';
+    const Y_ITEMS_ALIAS = 'items';
 
     /**
      * @param array $records
@@ -21,14 +22,30 @@ class GroupedLineChart extends AbstractChart
         $yPropertyName = $config['chart'][static::Y_AXIS_ALIAS];
 
         $extractedData = [];
-        $groupName = $config['chart'][static::Y_ITEMS_GROUP_NAME];
+        $groupName = '';
+        if (isset($config['chart'][static::Y_ITEMS_GROUP_NAME])) {
+            $groupName = $config['chart'][static::Y_ITEMS_GROUP_NAME];
+        }
 
         foreach ($records as $record) {
             $xData = is_object($record[$xPropertyName]) ? (string)$record[$xPropertyName] : $record[$xPropertyName];
-            $yData = is_object($record[$yPropertyName]) ? (string)$record[$yPropertyName] : $record[$yPropertyName];
-            $groupData = is_object($record[$groupName]) ? (string)$record[$groupName] : $record[$groupName];
+            if (!is_array($yPropertyName)) {
+                $yData = is_object($record[$yPropertyName]) ? (string)$record[$yPropertyName] : $record[$yPropertyName];
+            }
+            if ($groupName) {
+                $groupData = is_object($record[$groupName]) ? (string)$record[$groupName] : $record[$groupName];
+            }
 
-            $extractedData[$xData][$groupData] = $yData;
+            if (is_array($config['chart'][static::Y_AXIS_ALIAS])) {
+                if (isset($config['chart'][static::Y_AXIS_ALIAS][static::Y_ITEMS_ALIAS])) {
+                    foreach ($config['chart'][static::Y_AXIS_ALIAS][static::Y_ITEMS_ALIAS] as $yItem) {
+                        $extractedData[$xData][$yItem] = $record[$yItem];
+                    }
+                }
+            } else {
+                $extractedData[$xData][$groupData] = $yData;
+            }
+
         }
 
         return $extractedData;
@@ -36,10 +53,7 @@ class GroupedLineChart extends AbstractChart
 
     protected function validateParameters(array $config)
     {
-        if (!isset($config['chart'][static::X_AXIS_ALIAS]) ||
-            !isset($config['chart'][static::Y_AXIS_ALIAS]) ||
-            !isset($config['chart'][static::Y_ITEMS_GROUP_NAME])
-        ) {
+        if (!isset($config['chart'][static::X_AXIS_ALIAS]) || !isset($config['chart'][static::Y_AXIS_ALIAS])) {
             throw new \RuntimeException("Report has missed required parameters");
         }
         return true;
