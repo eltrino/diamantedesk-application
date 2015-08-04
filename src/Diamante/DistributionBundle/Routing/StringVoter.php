96db564d7c4cc4810232f454b3d38799ec89bced
Vladimir Polischuk <vladimir.polischuk@eltrino.com>
2015-08-04 11:22:59 +0300

    add route to whitelist

--- src/Diamante/DistributionBundle/Routing/StringVoter.php
+++ src/Diamante/DistributionBundle/Routing/StringVoter.php
@@ -49,6 +49,7 @@ class StringVoter extends Voter
             'oro_datagrid_mass_action',
             'oro_datagrid_widget',
             'oro_datagrid_index',
+            'oro_datagrid_api_rest_gridview_post',
             'oro_tag_search',
             'oro_security_access_levels',
             'oro_translation_available_translations',
