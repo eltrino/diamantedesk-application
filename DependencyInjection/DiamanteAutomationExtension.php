6654b3ba3185bd53bb15048b96c9c5c3fbb661e0
Vladimir Polischuk <vladimir.polischuk@eltrino.com>
2015-08-27 18:11:38 +0300

    Add configuration for listed entities

--- DependencyInjection/DiamanteAutomationExtension.php
+++ DependencyInjection/DiamanteAutomationExtension.php
@@ -2,18 +2,33 @@
 
 namespace Diamante\AutomationBundle\DependencyInjection;
 
+use Oro\Component\Config\Loader\CumulativeConfigLoader;
+use Oro\Component\Config\Loader\YamlCumulativeFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class DiamanteAutomationExtension extends Extension
+class DiamanteAutomationExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container)
+    {
+        $loader = new CumulativeConfigLoader(
+            'diamante_automation',
+            new YamlCumulativeFileLoader('Resources/config/automation.yml')
+        );
+
+        $resources  = $loader->load();
+
+        $this->populateAutomationEntities($container, $resources);
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -26,5 +41,17 @@ class DiamanteAutomationExtension extends Extension
         $loader->load('services.xml');
         $loader->load('actions.xml');
         $loader->load('strategies.xml');
+    }
+
+    private function populateAutomationEntities(ContainerBuilder $container, $resource)
+    {
+        $entities = [];
+        foreach ($resource as $item) {
+            if (isset($item->data['entities'])) {
+                $entities = array_merge($entities, $item->data['entities']);
+            }
+        }
+
+        $container->setParameter('diamante.automation.listed_entities', $entities);
     }
 }
