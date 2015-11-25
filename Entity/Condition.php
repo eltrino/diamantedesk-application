36ab77136898aadf20fb296569e830826a4cea7f
Alexandr Kolomiec <alexandr.kolomiec@eltrino.com>
2015-11-25 19:43:35 +0200

    DIAM-1327 create condition entity

---
+++ Entity/Condition.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Copyright (c) 2014 Eltrino LLC (http://eltrino.com)
+ *
+ * Licensed under the Open Software License (OSL 3.0).
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://opensource.org/licenses/osl-3.0.php
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@eltrino.com so we can send you a copy immediately.
+ */
+
+namespace Diamante\AutomationBundle\Entity;
+
+use Diamante\AutomationBundle\Model\Target;
+use Diamante\AutomationBundle\Rule\Condition\Condition as RuleCondition;
+use Diamante\DeskBundle\Model\Shared\Entity;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="Diamante\DeskBundle\Infrastructure\Persistence\DoctrineGenericRepository")
+ * @ORM\Table(name="diamante_rule_condition")
+ */
+class Condition extends \Diamante\AutomationBundle\Model\Condition
+{
+    /**
+     * @var \Rhumsaa\Uuid\Uuid
+     *
+     * @ORM\Id
+     * @ORM\Column(type="string", name="id")
+     * @ORM\GeneratedValue(strategy="NONE")
+     */
+    protected $id;
+
+    /**
+     * @var RuleCondition
+     * @ORM\Column(name="rule_condition", type="string")
+     */
+    protected $condition;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Group", inversedBy="conditions")
+     */
+    protected $group;
+}
