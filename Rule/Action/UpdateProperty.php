3d04573f873f63eccd4f6b1bb3e6cf3640282c76
Alexandr Kolomiec <alexandr.kolomiec@eltrino.com>
2015-12-03 22:12:06 +0200

    DIAM-1325 create update property action

---
+++ Rule/Action/UpdateProperty.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Copyright (c) 2015 Eltrino LLC (http://eltrino.com)
+ *
+ * Licensed under the Open Software License (OSL 3.0).
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://opensource.org/licenses/osl-3.0.php
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@eltrino.com so we can send you a copy immediately.
+ */
+
+namespace Diamante\AutomationBundle\Rule\Action;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
+class UpdateProperty extends AbstractAction
+{
+    /**
+     * @var Registry
+     */
+    private $registry;
+
+    /**
+     * @param Registry $registry
+     */
+    public function __construct(Registry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    public function execute()
+    {
+        $context = $this->getContext();
+        $target = $context->getFact()->getTarget();
+        $properties = $context->getParameters()->all();
+        $target->updateProperties($properties);
+        $this->registry->getManager()->persist($target);
+    }
+}
