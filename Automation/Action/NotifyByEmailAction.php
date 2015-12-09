afa763f4557f6bbb91c3b3fe34d0412695e81dad
Alexandr Kolomiec <alexandr.kolomiec@eltrino.com>
2015-12-09 21:18:07 +0200

    DIAM-1326 create notify action

---
+++ Automation/Action/NotifyByEmailAction.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ * Copyright (c) 2015 Eltrino LLC (http://eltrino.com)
+ *
+ * Licensed under the Open Software License (OSL 3.0).
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://opensource.org/licenses/osl-3.0.php
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@eltrino.com so we can send you a copy immediately.
+ */
+
+namespace Diamante\AutomationBundle\Automation\Action;
+
+use Diamante\AutomationBundle\Rule\Action\AbstractAction;
+use Diamante\DeskBundle\Infrastructure\Notification\NotificationManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Oro\Bundle\UserBundle\Entity\User as OroUser;
+use Diamante\DeskBundle\Entity\Ticket;
+use Diamante\DeskBundle\Entity\Comment;
+
+class NotifyByEmailAction extends AbstractAction
+{
+    /**
+     * @var NotificationManager
+     */
+    private $notificationManager;
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @param NotificationManager $notificationManager
+     * @param ContainerInterface  $container
+     */
+    public function __construct(NotificationManager $notificationManager, ContainerInterface $container)
+    {
+        $this->notificationManager = $notificationManager;
+        $this->container = $container;
+    }
+
+    public function execute()
+    {
+        /**
+         * TODO get $action and $emails vars form fact
+         */
+        $action = 'created';
+        $emails = ['akolomiec1989@gmail.com', 'mike@diamantedesk.com'];
+        $fact = $this->getContext()->getFact();
+        $target = $fact->getTarget();
+        $targetType = $fact->getTargetType();
+        $provider = sprintf('%s_%s', $targetType, $action);
+
+        /**
+         * TODO get changes and attachments form fact
+         */
+        $options = [
+            'ticketKey' => $this->getTicketKey($target),
+            'changes' => [],
+            'attachments' => []
+        ];
+        foreach ($emails as $email) {
+            $recipient = $this->container->get('diamante.user.service')->getUserInstanceByEmail($email);
+            $options['isOroUser'] = false;
+
+            /**
+             * TODO determine user in render_ticket_url twig extension
+             */
+            if ($recipient instanceof OroUser) {
+                $options['isOroUser'] = true;
+            }
+
+            $this->notificationManager->notifyByScenario($provider, $recipient, $options);
+        }
+    }
+
+    private function getTicketKey($target)
+    {
+        if ($target instanceof Ticket) {
+            return $target->getKey();
+        } elseif ($target instanceof Comment) {
+            return $target->getTicket()->getKey();
+        }
+
+        return null;
+    }
+}
---
+++ Infrastructure/Notification/OptionProvider/TicketCreated.php
@@ -0,0 +1,123 @@
+<?php
+/*
+ * Copyright (c) 2015 Eltrino LLC (http://eltrino.com)
+ *
+ * Licensed under the Open Software License (OSL 3.0).
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://opensource.org/licenses/osl-3.0.php
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@eltrino.com so we can send you a copy immediately.
+ */
+
+namespace Diamante\AutomationBundle\Infrastructure\Notification\OptionProvider;
+
+
+use Diamante\DeskBundle\Infrastructure\Notification\NotificationOptionsProvider;
+use Diamante\UserBundle\Entity\DiamanteUser;
+use Diamante\DeskBundle\Model\Ticket\EmailProcessing\Services\MessageReferenceServiceImpl;
+
+class TicketCreated implements NotificationOptionsProvider
+{
+    const HTML_TEMPLATE = 'DiamanteAutomationBundle:Entity:Notification/notification.html.twig';
+    const TXT_TEMPLATE  = 'DiamanteAutomationBundle:Entity:Notification/notification.txt.twig';
+
+    const SUBJECT_IDENTIFIER = 'diamante.automation.notification.ticket.created';
+    const NAME = 'ticket_created';
+
+    /**
+     * @var DiamanteUser
+     */
+    protected $recipient;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return self::NAME;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHtmlTemplate()
+    {
+        return self::HTML_TEMPLATE;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTxtTemplate()
+    {
+        return self::TXT_TEMPLATE;
+    }
+
+    /**
+     * @param $recipient
+     */
+    public function setRecipient($recipient)
+    {
+        $this->recipient = $recipient;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRecipientEmail()
+    {
+        return $this->recipient->getEmail();
+    }
+
+    /**
+     * @return string
+     */
+    public function getRecipientName()
+    {
+        if ($this->recipient instanceof DiamanteUser) {
+            return $this->recipient->getFullName();
+        } else {
+            return $this->recipient->getFirstName() . ' ' . $this->recipient->getLastName();
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubject()
+    {
+        return self::SUBJECT_IDENTIFIER;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRequiredParams()
+    {
+        return ['delimiter', 'header', 'user', 'changes', 'attachments', 'ticketKey'];
+    }
+
+    /**
+     * @return bool
+     */
+    public function subjectIsTranslatable()
+    {
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function getDefaultOptions()
+    {
+        return [
+            'delimiter' => MessageReferenceServiceImpl::DELIMITER_LINE,
+            'header' => 'diamante.automation.notification.ticket.created',
+            'user' => $this->getRecipientName()
+        ];
+    }
+}
