1b7e72fb3c50584b33bff7b4d19a8875fef35369
Alexandr Kolomiec <alexandr.kolomiec@eltrino.com>
2016-04-06 19:10:51 +0300

    DIAM-1462 fix default branch configuration

---
+++ src/Diamante/DeskBundle/EventListener/SettingsListener.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Copyright (c) 2014 Eltrino LLC (http://eltrino.com)
+ *
+ * Licensed under the Open Software License (OSL 3.0).
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://opensource.org/licenses/osl-3.0.php
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@eltrino.com so we can send you a copy immediately.
+ */
+namespace Diamante\DeskBundle\EventListener;
+
+use Oro\Bundle\ConfigBundle\Event\ConfigSettingsUpdateEvent;
+
+/**
+ * Class SettingsListener
+ *
+ * @package Diamante\DeskBundle\EventListener
+ */
+class SettingsListener
+{
+    const DIAMANTE_DEFAULT_BRANCH = 'diamante_desk___default_branch';
+
+    /**
+     * @param ConfigSettingsUpdateEvent $event
+     */
+    public function beforeSave(ConfigSettingsUpdateEvent $event)
+    {
+        $setting = $event->getSettings();
+        $setting[static::DIAMANTE_DEFAULT_BRANCH]['use_parent_scope_value'] = false;
+        $event->setSettings($setting);
+    }
+}
