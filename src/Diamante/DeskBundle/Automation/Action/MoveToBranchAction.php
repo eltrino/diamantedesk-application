0ca85b1f1c678dea98090c4e4810b9bdac2eb598
Alexandr Kolomiec <alexandr.kolomiec@eltrino.com>
2016-04-18 17:58:46 +0300

    DIAM-1659 fix ticket history for move to branch action

--- src/Diamante/DeskBundle/Automation/Action/MoveToBranchAction.php
+++ src/Diamante/DeskBundle/Automation/Action/MoveToBranchAction.php
@@ -17,6 +17,8 @@ namespace Diamante\DeskBundle\Automation\Action;
 
 use Diamante\AutomationBundle\Rule\Action\AbstractModifyAction;
 use Diamante\DeskBundle\Entity\Ticket;
+use Diamante\DeskBundle\Entity\TicketHistory;
+use Doctrine\DBAL\LockMode;
 use Proxies\__CG__\Diamante\DeskBundle\Entity\Branch;
 
 /**
@@ -32,8 +34,6 @@ class MoveToBranchAction extends AbstractModifyAction
     {
         $branchId = null;
         $target = $this->context->getFact()->getTarget();
-        /** @var Ticket $ticket */
-        $ticket = $this->em->getRepository('DiamanteDeskBundle:Ticket')->get($target['id']);
 
         if ($this->context->getParameters()->has(static::ACTION_NAME)) {
             $branchId = $this->context->getParameters()->all()[static::ACTION_NAME];
@@ -45,11 +45,21 @@ class MoveToBranchAction extends AbstractModifyAction
 
         /** @var Branch $branch */
         $branch = $this->em->getRepository('DiamanteDeskBundle:Branch')->get($branchId);
+        $this->em->getConnection()->beginTransaction();
 
-        $ticket->move($branch);
+        try {
+            /** @var Ticket $ticket */
+            $ticket = $this->em->getRepository('DiamanteDeskBundle:Ticket')->get($target['id']);
+            $this->em->lock($branch, LockMode::PESSIMISTIC_READ);
+            $this->em->getRepository('DiamanteDeskBundle:TicketHistory')->store(new TicketHistory($ticket));
+            $ticket->move($branch);
 
-        $this->disableListeners();
-        $this->em->persist($ticket);
-        $this->em->flush();
+            $this->disableListeners();
+            $this->em->persist($ticket);
+            $this->em->flush();
+            $this->em->getConnection()->commit();
+        } catch (\Exception $e) {
+            $this->em->getConnection()->rollback();
+        }
     }
 }
