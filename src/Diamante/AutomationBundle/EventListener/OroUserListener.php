d32e9eb6942ad9787ff87d425ab94b2568897416
Alexandr Kolomiec <alexandr.kolomiec@eltrino.com>
2016-07-13 19:46:26 +0300

    DIAM-1752 make property unassigned if user was removed

---
+++ src/Diamante/AutomationBundle/EventListener/OroUserListener.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * Copyright (c) 2014 Eltrino LLC (http://eltrino.com)
+ *
+ * Licensed under the Open Software License (OSL 3.0).
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://opensource.org/licenses/osl-3.0.php
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@eltrino.com so we can send you a copy immediately.
+ */
+namespace Diamante\AutomationBundle\EventListener;
+
+use Diamante\AutomationBundle\Automation\Action\UpdatePropertyAction;
+use Diamante\AutomationBundle\Entity\Action;
+use Diamante\UserBundle\Model\User;
+use Oro\Bundle\UserBundle\Entity\User as OroUser;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+class OroUserListener
+{
+    /**
+     * @param LifecycleEventArgs $eventArgs
+     */
+    public function preRemove(LifecycleEventArgs $eventArgs)
+    {
+        $manager = $eventArgs->getEntityManager();
+        $entity = $eventArgs->getEntity();
+
+        if (!$entity instanceof OroUser) {
+            return;
+        }
+
+        $workflowActions = $manager->getRepository('DiamanteAutomationBundle:WorkflowAction')->findByType(
+            UpdatePropertyAction::UPDATE_PROPERTY_ACTION
+        );
+        $businessActions = $manager->getRepository('DiamanteAutomationBundle:BusinessAction')->findByType(
+            UpdatePropertyAction::UPDATE_PROPERTY_ACTION
+        );
+
+        /** @var Action[] $actions */
+        $actions = array_merge($workflowActions, $businessActions);
+
+        foreach ($actions as $action) {
+            $parameters = $action->getParameters();
+
+            if (array_key_exists('assignee', $parameters)) {
+                $user = User::fromString($parameters['assignee']);
+
+                if ($user->getId() == $entity->getId()) {
+                    $parameters['assignee'] = UpdatePropertyAction::UNASSIGNED;
+                    $action->setParameters($parameters);
+                    $manager->persist($action);
+                }
+            }
+        }
+    }
+}
