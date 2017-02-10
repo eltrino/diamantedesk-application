ba15b0cf437d8c7af8ccd301fe6e72e4c4a23687
Vladimir Polischuk <pol.vova@gmail.com>
2017-02-10 15:39:26 +0200

    DIAM-1836 Attachment not available on email notifiaction

---
+++ src/Diamante/AutomationBundle/Infrastructure/Changeset/FieldProcessor/AttachmentsProcessor.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Copyright (c) 2015 Eltrino LLC (http://eltrino.com)
+ *
+ * Licensed under the Open Software License (OSL 3.0).
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://opensource.org/licenses/osl-3.0.php
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@eltrino.com so we can send you a copy immediately.
+ */
+
+namespace Diamante\AutomationBundle\Infrastructure\Changeset\FieldProcessor;
+
+use Diamante\AutomationBundle\Infrastructure\Changeset\FieldProcessor;
+use Diamante\DeskBundle\Entity\Attachment;
+
+/**
+ * Class ChangesetBuilder
+ *
+ * @package Diamante\AutomationBundle\Infrastructure\Changeset
+ */
+class AttachmentsProcessor implements FieldProcessor
+{
+
+    /**
+     * @param $input
+     * @return mixed
+     */
+    public function processField($input)
+    {
+        $output = [];
+        /** @var Attachment $attachment */
+        foreach ($input as $attachment) {
+            $output[] = $attachment->getHash();
+        }
+
+        return $output;
+    }
+}
